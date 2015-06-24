9097903b97ccdbc7048c9902cf4a27a4ed372099
Lauren Thomson <LT@Laurens-MacBook-Pro.local>
2015-06-24 19:13:26 -0400

    added Angry boss program

---
+++ angry.rb
@@ -0,0 +1,4 @@
+puts "What the hell do you want now you lazy slob?"
+answer = gets.chomp
+"I want " + answer
+puts  "ARE YOU FREAKIN KIDDING? YOU GOT " + answer.upcase + " 14 YEARS AGO!! YOU'RE FIRED!"
